39a67d1df3f5d23986f277992007639001b6eb37
Jason Carney <jrcarney.16@gmail.com>
2014-12-05 16:05:04 +0000

    Menu edits

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -14,8 +14,8 @@
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="index.php">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="/about.php">About</a></li>
+						<li><a href="/contact.php">Contact</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
