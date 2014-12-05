175049c68a24af1243e6442b585325370dc28f93
lairdthompson <david@lairdthompson.com>
2014-12-05 12:33:37 +0000

    updated image

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
     		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     	</div>
     	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" data-src="http://picttwo.com/wp-content/uploads/2014/10/cute-animal-pictures.jpg" height="500" width="500" alt="Kitten">
+    		<img class="featurette-image img-responsive" src="http://picttwo.com/wp-content/uploads/2014/10/cute-animal-pictures.jpg" height="500" width="500" alt="Kitten">
     	</div>
     </div>
 
