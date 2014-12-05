c42d219ff3a0090b90b07bb45512cd9db60f2aac
Karolis Danilevicius <karolisd@hotmail.lt>
2014-12-05 15:46:28 +0000

    another_action.php update

--- another_action.php
+++ another_action.php
@@ -17,32 +17,7 @@
     		<p class="lead">Version control management? Oh, tell me more...</p>
     	</div>
     	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" src="http://media.giphy.com/media/ZKA0VSVuFeeBO/giphy.gif" height="500" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-    	</div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-    	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" src="https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-    	</div>
-    	<div class="col-md-7">
-    		<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-    		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-    	</div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-    	<div class="col-md-7">
-    		<h2 class="featurette-heading">This horse loves <span class="text-muted">Git</span></h2>
-    		<p class="lead">Look at that horse! This is one happy, badass horse who started using git version management control.</p>
-    	</div>
-    	<div class="col-md-5">
-
-    		<img class="featurette-image img-responsive" src="http://www.horseloverslodge.com/images/image7.jpeg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    		<img class="featurette-image img-responsive" src="http://media.giphy.com/media/ZKA0VSVuFeeBO/giphy.gif" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
     	</div>
     </div>
 
