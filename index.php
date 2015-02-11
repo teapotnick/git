30d65e955fc82e025c1ba0431a3c7ee1371258d4
Craig Francis <craig@craigfrancis.co.uk>
2015-02-11 12:06:24 +0000

    Use counter without +1

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide" />
 				<div class="container">
 					<div class="carousel-caption">
-						<h1>Slide <?= html($counter+1) ?> Heading</h1>
+						<h1>Slide <?= html($counter) ?> Heading</h1>
 						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 						<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
 					</div>
