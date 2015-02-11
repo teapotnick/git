4129beabe50ee97539ecc37776eac2437e4973ed
James <james.stacey@imaginet.co.uk>
2015-02-11 14:48:18 +0000

    Change to navigation

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -9,13 +9,21 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Project name</a>
+					<a class="navbar-brand" href="#">Git Development Test</a>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="/">Home</a></li>
 						<li><a href="/about/">About</a></li>
 						<li><a href="/#contact">Contact</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Our Work<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Who We Are</a></li>
+								<li><a href="#">What We Do</a></li>
+								<li><a href="#">How We Do It</a></li>
+							</ul>
+						</li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -28,6 +36,7 @@
 								<li><a href="#">One more separated link</a></li>
 							</ul>
 						</li>
+                                                <li><a href="/#sitemap">Site Map</a></li>						
 					</ul>
 				</div>
 			</div>
