8414565b7b0db999abc6eef76724702dff03cd53
Craig Francis <craig@craigfrancis.co.uk>
2015-02-11 12:01:52 +0000

    Update css/js links to absolute URLs

--- template-parts/header.php
+++ template-parts/header.php
@@ -48,7 +48,7 @@
 		<title>Git Exercise</title>
 
 		<!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link href="/css/bootstrap.min.css" rel="stylesheet" />
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -57,9 +57,9 @@
 		<![endif]-->
 
 		<!-- Custom styles for this template -->
-		<link href="css/carousel.css" rel="stylesheet" />
-		<link href="css/customisations.css" rel="stylesheet" />
-		<script src="js/holder.js"></script>
+		<link href="/css/carousel.css" rel="stylesheet" />
+		<link href="/css/customisations.css" rel="stylesheet" />
+		<script src="/js/holder.js"></script>
 	</head>
 <!-- NAVBAR
 ================================================== -->
