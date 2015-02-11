17fda6745fa2d131f7042c80a668101bb7c3c2cc
Craig Francis <craig@craigfrancis.co.uk>
2015-02-11 11:21:29 +0000

    Enable XML mode output

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     </ol>
     <div class="carousel-inner">
     	<div class="item active">
-    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide" />
     		<div class="container">
     			<div class="carousel-caption">
     				<h1>Slide 1 Heading</h1>
@@ -21,7 +21,7 @@
     		</div>
     	</div>
     	<div class="item">
-    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide" />
     		<div class="container">
     			<div class="carousel-caption">
     				<h1>Slide 2 Heading</h1>
@@ -31,7 +31,7 @@
     		</div>
     	</div>
     	<div class="item">
-    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+    		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide" />
     		<div class="container">
     			<div class="carousel-caption">
     				<h1>Slide 3 Heading</h1>
@@ -56,29 +56,29 @@
     <!-- Three columns of text below the carousel -->
     <div class="row">
     	<div class="col-lg-4">
-    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;" />
     		<h2>Heading</h2>
     		<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-    		<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    		<p><a class="btn btn-default" href="#" role="button">View details »</a></p>
     	</div><!-- /.col-lg-4 -->
     	<div class="col-lg-4">
-    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;" />
     		<h2>Heading</h2>
     		<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-    		<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    		<p><a class="btn btn-default" href="#" role="button">View details »</a></p>
     	</div><!-- /.col-lg-4 -->
     	<div class="col-lg-4">
-    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+    		<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;" />
     		<h2>Heading</h2>
     		<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-    		<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    		<p><a class="btn btn-default" href="#" role="button">View details »</a></p>
     	</div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
 
 
     <!-- START THE FEATURETTES -->
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider" />
 
     <div class="row featurette">
     	<div class="col-md-7">
@@ -86,15 +86,15 @@
     		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     	</div>
     	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
     	</div>
     </div>
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider" />
 
     <div class="row featurette">
     	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
     	</div>
     	<div class="col-md-7">
     		<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
@@ -102,7 +102,7 @@
     	</div>
     </div>
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider" />
 
     <div class="row featurette">
     	<div class="col-md-7">
@@ -110,11 +110,11 @@
     		<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     	</div>
     	<div class="col-md-5">
-    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    		<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
     	</div>
     </div>
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider" />
 
     <!-- /END THE FEATURETTES -->
 
@@ -122,7 +122,7 @@
     <!-- FOOTER -->
     <footer>
     	<p class="pull-right"><a href="#">Back to top</a></p>
-    	<p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    	<p>© 2014 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
     </footer>
 
 </div><!-- /.container -->
--- template-parts/header.php
+++ template-parts/header.php
@@ -1,17 +1,54 @@
+<?php
+
+//--------------------------------------------------
+// Config
+
+	define('CHARSET', 'UTF-8');
+	define('LANG', 'en');
+
+//--------------------------------------------------
+// Mime type
+
+	$request_accept = (isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '');
+
+	$mime_xml = 'application/xhtml+xml';
+	if (stripos($request_accept, $mime_xml) === false) {
+		$mime_type = 'text/html';
+	} else {
+		$mime_type = $mime_xml;
+	}
+
+//--------------------------------------------------
+// Support functions
+
+	function html($text) {
+		return htmlspecialchars($text, ENT_QUOTES, CHARSET); // htmlentities does not work for HTML5+XML
+	}
+
+	function head($text) {
+		return str_replace(array("\r", "\n", "\0"), '', $text);
+	}
+
+//--------------------------------------------------
+// Headers
+
+	header('Content-Type: ' . head($mime_type) . '; charset=' . head(CHARSET));
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= html(LANG) ?>" xml:lang="<?= html(LANG) ?>" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="icon" href="/favicon.ico">
+		<meta charset="<?= html(CHARSET) ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<link rel="icon" href="/favicon.ico" />
 
 		<title>Git Exercise</title>
 
 		<!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -20,8 +57,8 @@
 		<![endif]-->
 
 		<!-- Custom styles for this template -->
-		<link href="css/carousel.css" rel="stylesheet">
-		<link href="css/customisations.css" rel="stylesheet">
+		<link href="css/carousel.css" rel="stylesheet" />
+		<link href="css/customisations.css" rel="stylesheet" />
 		<script src="js/holder.js"></script>
 	</head>
 <!-- NAVBAR
