530fd48faf2926eb0d60bffd52031dc98aa5933e
Craig Francis <craig@craigfrancis.co.uk>
2015-02-11 12:00:33 +0000

    Add about page

---
+++ about/index.php
@@ -0,0 +1,17 @@
+<?php
+	include('../template-parts/header.php');
+?>
+
+<div class="container marketing">
+
+	<p>Hello</p>
+
+	<!-- FOOTER -->
+	<footer>
+		<p class="pull-right"><a href="#">Back to top</a></p>
+		<p>© 2014 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+	</footer>
+
+</div><!-- /.container -->
+
+<?php include('../template-parts/footer.php'); ?>
