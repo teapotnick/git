ebcf0c5460829c35aaa16c4d030f3e102e7ba64e
Craig Francis <craig@craigfrancis.co.uk>
2015-02-11 15:27:01 +0000

    Kill holder.js

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
 			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
 		</div>
 		<div class="col-md-5">
-			<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+			<img class="featurette-image img-responsive" src="https://placekitten.com/g/500/500" alt="Generic placeholder image" />
 		</div>
 	</div>
 
@@ -97,7 +97,7 @@
 
 	<div class="row featurette">
 		<div class="col-md-5">
-			<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+			<img class="featurette-image img-responsive" src="https://placekitten.com/g/500/500" alt="Generic placeholder image" />
 		</div>
 		<div class="col-md-7">
 			<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
@@ -113,7 +113,7 @@
 			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
 		</div>
 		<div class="col-md-5">
-			<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+			<img class="featurette-image img-responsive" src="https://placekitten.com/g/500/500" alt="Generic placeholder image" />
 		</div>
 	</div>
 
