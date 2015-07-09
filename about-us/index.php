8d8ee1e6effaf7a2384ddc5b71c4694be85472c0
Chris Baines <chris@createmarketing.co.uk>
2015-07-09 11:43:14 +0100

    Pie image added to about us page

--- about-us/index.php
+++ about-us/index.php
@@ -3,7 +3,9 @@
 <div class="container theme-showcase" role="main">
 	<!-- Main jumbotron for a primary marketing message or call to action -->
 	<div class="jumbotron">
+    	<p>&nbsp;</p>
         <h1>Jumbotron heading</h1>
+        <img src="http://thesensiblemom.com/wp-content/uploads/2013/10/applepie1.png" />
         <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
       </div>
