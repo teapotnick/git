a2d8cf5d9ffacf03543927f19973c5207588f558
Oli Ward <oliward@gmail.com>
2015-07-09 11:15:01 +0100

    remove footer again

--- index.php
+++ index.php
@@ -153,13 +153,6 @@
 
     <!-- /END THE FEATURETTES -->
 
-
-    <!-- FOOTER -->
-    <footer>
-    	<p class="pull-right"><a href="#">Back to top</a></p>
-    	<p>&copy; 2015 Create Marketing. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-    </footer>
-
 </div><!-- /.container -->
 
 <?php include('template-parts/footer.php'); ?>
