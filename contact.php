db1372af6d6743bb558d2a0bb7546fdfbb2e016f
Rob <rk21980@gmail.com>
2015-11-16 11:20:31 +0000

    working on the contact page

--- contact.php
+++ contact.php
@@ -1,2 +1,11 @@
 <?php
-echo "get in touch with Santa";
+// YYY
+include('template-parts/header.php'); ?>
+
+// contact form goes here
+?>
+<body>
+	<p>get in touch with Santa</p>
+</body>
+
+<?php include('template-parts/footer.php'); ?>
