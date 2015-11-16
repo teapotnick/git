3db1df87bbcba23cb29c9fcb422709e13ce3a418
jaufwa <jonathan.miles@hotmail.co.uk>
2015-11-16 12:45:20 +0000

    order page

---
+++ order.php
@@ -0,0 +1,30 @@
+<?php include('template-parts/header.php'); ?>
+
+<div class="row">
+	<div class="col-lg-12">
+		<h1>Order Page</h1>
+		<p>Use this page to order the items advertised on the site</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg4">
+		<img src="/images/ps4.png">
+	</div>
+	<div class="col-lg8">
+		<h2>PS4</h2>
+		<h3>£400</h3>
+		<p>Order the PS4 now and receieve a tonne of peripherals such as light gun, steering wheel, dance mat, dodgy camera that spies on you etc.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg4">
+		<img src="/images/robot_buy.jpg">
+	</div>
+	<div class="col-lg8">
+		<h2>Killer Robot</h2>
+		<h3>£400</h3>
+		<p>Remember to specify on the cart page if you want this in mecha suit form or standard remote control robot.</p>
+	</div>
+</div>
+
+<?php include('template-parts/footer.php'); ?>
