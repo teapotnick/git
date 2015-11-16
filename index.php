6d758f1a13f1266393ddc71684ae4518e0a5a6e6
Richard Grimmer <Richard Grimmer>
2015-11-16 11:19:51 +0000

    Added a naff CSS Snwing effect....no idea how to hook it up, but this is about using GIT right?

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+
+echo '<link rel="stylesheet" type="text/css" href="css/SnowingEffect.css">';
 // YYY
 include('template-parts/header.php'); ?>
 
-<!-- Carousel
+  <!-- Carousel
 ================================================== -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
