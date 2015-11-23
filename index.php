d6758fe5a8c60c60804769fc0284876bff34cbfc
Dan Orchard <danorchard@Users-MacBook-Pro.local>
2015-11-23 15:12:57 +0000

    D:

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
     		<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     	</div><!-- /.col-lg-4 -->
         <div class="col-lg-3">
-    		<img class="img-circle" src="/img/Ade.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+    		<img class="img-circle" src="http://i.imgur.com/qURv1N9.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
     		<h2>Ade</h2>
     		<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
     		<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
