e8389d72d5347ff8097e02e04ca4fc7c8923abf9
Alex Conniff <alex@Alex.local>
2015-11-23 12:13:21 +0000

    footer change

--- index.php
+++ index.php
@@ -124,6 +124,7 @@
     <footer>
     	<p class="pull-right"><a href="#">Back to top</a></p>
     	<p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>Footer Text!</p>
     </footer>
 
 </div><!-- /.container -->
