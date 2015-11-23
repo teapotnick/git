6f12f35b0aec08d56290567f5d4a8541b17a86fc
Callum Bridgford-Whittick <callum@Callums-iMac.local>
2015-11-23 14:56:51 +0000

    James

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -3,5 +3,6 @@
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">do not click</a>
 	</body>
 </html>
