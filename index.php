24bc37123de7480a3eb32562e967fdb2f3e8bbb9
teapotnickc <nick.c@teapotcreative.co.uk>
2015-11-23 14:25:10 +0000

    third home slider change

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     			</div>
     		</div>
     	</div>
-    	
+    	</div>
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 </div><!-- /.carousel -->
