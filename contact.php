426ad2ff0fe6262e924a40f9cbbea01837852cd6
Antony Hyam <antony@teapotcreative.co.uk>
2015-11-23 14:02:53 +0000

    styling form

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php include('template-parts/header.php'); ?>
-<div class="container">
+<div class="container contact-page">
     
     
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2510.854706892908!2d-2.8614776842486123!3d51.000356979555114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48720d6865c70499%3A0xd6cd1e257ef499af!2sTeapot+Creative!5e0!3m2!1sen!2suk!4v1448286827698" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
